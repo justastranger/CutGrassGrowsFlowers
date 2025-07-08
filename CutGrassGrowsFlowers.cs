0bef58ea7986ac0ae5e68d1ea44ffdb7b7c863b6
justastrangerlica <justastrangerlica@gmail.com>
2025-07-08 16:17:06 -0500

    make wildflower list readonly

--- CutGrassGrowsFlowers.cs
+++ CutGrassGrowsFlowers.cs
@@ -30,7 +30,7 @@ namespace CutGrassGrowsFlowers
     [HarmonyPatch("WorldManager", "nextDayChanges")]
     public class WorldManagerPatch
     {
-        private static List<int> wildFlowers = new List<int> { 201, 202, 203, 204, 205, 498, 499, 500, 516, 517, 518, 521, 522, 523, 573, 574, 575, 783, 784, 785, 786, 787 };
+        private static readonly List<int> wildFlowers = new List<int> { 201, 202, 203, 204, 205, 498, 499, 500, 516, 517, 518, 521, 522, 523, 573, 574, 575, 783, 784, 785, 786, 787 };
 
         public static void Postfix(WorldManager __instance, bool raining, int mineSeed)
         {
